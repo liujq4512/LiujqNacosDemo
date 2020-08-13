88e0527c5cf611b98055e81e5b0eb0e9989b485d
liujq4512 <814780995@qq.com>
2020-08-13 15:06:16 +0800

    feign retry ok

--- serviceA/src/main/java/com/nacos/controller/HelloWorldController.java
+++ serviceA/src/main/java/com/nacos/controller/HelloWorldController.java
@@ -1,15 +1,20 @@
 package com.nacos.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class HelloWorldController {
+//    Logger log=Logger.getLogger(HelloWorldController.class);
 
     @RequestMapping("/hello")
     public String hello(String userName) throws InterruptedException {
-        Thread.sleep(10000);
-        return "hello "+ userName;
-//        throw new RuntimeException("Test feign fallback exception");
+        log.info("--------------- ServiceA : hello world!!! ");
+//        Thread.sleep(10000);
+//        return "hello "+ userName;
+        throw new RuntimeException("ServiceA throws a exception");
     }
 }
--- serviceDemob/src/main/java/com/service/demob/ServiceDemobApplication.java
+++ serviceDemob/src/main/java/com/service/demob/ServiceDemobApplication.java
@@ -3,7 +3,9 @@ package com.service.demob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @EnableFeignClients
 @SpringBootApplication
 public class ServiceDemobApplication {
